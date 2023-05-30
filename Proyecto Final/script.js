e8352b9e339a82783844e4f24648f2905258f6c9
AlanQuispe13 <130708484+AlanQuispe13@users.noreply.github.com>
2023-05-30 10:19:25 -0300

    Add files via upload

---
+++ Proyecto Final/script.js
@@ -0,0 +1,22 @@
+function verificarEdad() {
+  var edad = prompt("Por favor, ingrese su edad:");
+
+  if (edad >= 18) {
+    alert("Bienvenido/a. Puede acceder al sitio.");
+  } else {
+    alert("Este sitio contiene contenido para adultos. Usted ingresa bajo su responsabilidad.");
+  }
+}
+
+verificarEdad();
+
+function mostrarAlerta() {
+  var selectElement = document.getElementById("mySelect");
+  var selectedValue = selectElement.value;
+  
+  if (selectedValue === "") {
+    alert("Por favor, seleccione una opción");
+  } else {
+    alert("El costo del envio es: " + selectedValue);
+  }
+}
