e85804afc1c6831b5bad2e175ca7df40b9daec8f
AlanQuispe13 <130708484+AlanQuispe13@users.noreply.github.com>
2023-05-31 23:47:29 -0300

    Delete Proyecto Final directory

--- Proyecto Final/script2.js
+++
@@ -1,9 +0,0 @@
-
-const navbarMenu = document.getElementById('navbar-menu');
-function toggleNavbarMenu() {
-  navbarMenu.classList.toggle('hidden');
-}
-
-window.onbeforeunload = function() {
-    return "¡Atención! Estás abandonando el sitio. ¿Estás seguro?";
-  };
